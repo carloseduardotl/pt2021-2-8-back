f83d8a78c8ef0bf5212a9c6de7d3f0e6afb93d5a
Carlos Eduardo Taborda Lottermann <cadutaborda@gmail.com>
2021-11-28 17:11:28 -0300

    feat: CRUD de tags criado
    Implementado com o rails generate Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_28_194011) do
+ActiveRecord::Schema.define(version: 2021_11_28_200442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2021_11_28_194011) do
     t.string "name"
     t.text "content"
     t.integer "views"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
